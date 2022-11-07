6f07ceb958b92de019515f46ae9c2398161d8805
Ahmed Rajib <ahmedrajibbb@gmail.com>
2022-11-07 15:11:49 +0600

    Initial Commit

---
+++ MakeVideoFromImages/MakeVideoFromImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakeVideoFromImagesApp.swift
+//  MakeVideoFromImages
+//
+//  Created by Ahmed Rajib on 7/11/22.
+//
+
+import SwiftUI
+
+@main
+struct MakeVideoFromImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
